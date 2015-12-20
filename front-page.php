93e03d4ea5c0e104002efc583e9a58f52ccca1e2
Blake Cerecero <Blake@DigitalBlake.com>
2015-12-20 00:09:04 -0600

    [php] Include front-page.php template
    Signed-off-by: Blake Cerecero <Blake@DigitalBlake.com>

---
+++ front-page.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package wp_foundation_six
+ */
+
+get_header(); ?>
+
+	<?php wp_foundation_six_dev_helper( pathinfo(__FILE__, PATHINFO_FILENAME) ); ?>
+
+	<div class="container">
+
+		<main class="main" role="main" id="content">
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+
+			<?php endwhile; // End of the loop. ?>
+		</main>
+
+		<?php get_sidebar(); ?>
+
+	</div>
+
+<?php get_footer(); ?>
