45342a5450ece192b929c0c03c716f782aaa90cb
Blake Cerecero <Blake@DigitalBlake.com>
2016-01-19 23:52:34 -0600

    [DEV] Reorganized theme without vagrant to be used within vagrant instead; with composer
    Signed-off-by: Blake Cerecero <Blake@DigitalBlake.com>

--- wp-config.php
+++ wp-config.php
@@ -4,12 +4,12 @@ ini_set( 'display_errors', 0 );
 // ===================================================
 // Load database info and local development parameters
 // ===================================================
-if ( file_exists( dirname( __FILE__ ) . '/../production-config.php' ) ) {
+if ( file_exists( dirname( __FILE__ ) . '/production-config.php' ) ) {
     define( 'WP_LOCAL_DEV', false );
-    include( dirname( __FILE__ ) . '/../production-config.php' );
+    include( dirname( __FILE__ ) . '/production-config.php' );
 } else {
     define( 'WP_LOCAL_DEV', true );
-    include( dirname( __FILE__ ) . '/../local-config.php' );
+    include( dirname( __FILE__ ) . '/local-config.php' );
 }
 
 // ========================
