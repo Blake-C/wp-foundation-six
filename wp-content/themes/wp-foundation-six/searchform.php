d9d573d161668e69c9fe65beed71fda52a107ce3
Blake Cerecero <Blake@DigitalBlake.com>
2016-03-27 06:52:44 -0500

    [php] Fixed search form bug
    READ: https://developer.wordpress.org/reference/functions/get_search_form/
    Signed-off-by: Blake Cerecero <Blake@DigitalBlake.com>

--- wp-content/themes/wp-foundation-six/searchform.php
+++ wp-content/themes/wp-foundation-six/searchform.php
@@ -5,13 +5,11 @@
 <form role="search" method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<div class="row collapse">
 		<div class="small-8 columns">
-			<label class="show-for-sr" for="search">Search</label>
-			<input type="text" placeholder="Search..." value="<?php echo get_search_query(); ?>" name="search" id="search" />
+			<label class="show-for-sr" for="s">Search</label>
+			<input type="text" placeholder="Search..." value="<?php echo get_search_query(); ?>" name="s" id="s" />
 		</div>
 		<div class="small-4 columns">
 			<input type="submit" class="button expanded" id="searchsubmit" value="<?php echo esc_attr_x( 'Search', 'submit button' ); ?>" />
 		</div>
 	</div>
 </form>
-
-
