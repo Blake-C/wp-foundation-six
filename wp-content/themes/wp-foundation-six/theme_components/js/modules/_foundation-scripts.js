d0f42e56ea1c7217d0852ce6efd3e7dd40526811
Blake Cerecero <Blake@DigitalBlake.com>
2016-12-03 22:36:54 -0600

    [js] Moved foundation scripts to it’s own file
    Signed-off-by: Blake Cerecero <Blake@DigitalBlake.com>

---
+++ wp-content/themes/wp-foundation-six/theme_components/js/modules/_foundation-scripts.js
@@ -0,0 +1,27 @@
+/*************** Foundation Scripts ***************/
+/**
+ * Read the foundation docs on what to include for different
+ * foundation components
+ *
+ * @link http://foundation.zurb.com/sites/docs/javascript.html
+ *
+ * If you need a components for a single page then create another JS file,
+ * inlude it within the scripts-list.js file and import the needed assets.
+ * Then use the wp_enqueue_script to inlude it on the page you need through
+ * the functions.php file.
+ *
+ * Also, consider code splitting for keeping the initial size of the JS
+ * loaded on the page down:
+ *
+ * @link https://webpack.github.io/docs/code-splitting.html
+ */
+import '../../../node_modules/foundation-sites/js/foundation.core.js';
+import '../../../node_modules/foundation-sites/js/foundation.util.mediaQuery.js';
+import '../../../node_modules/foundation-sites/js/foundation.responsiveToggle.js';
+
+(function($){
+	$(document).foundation();
+
+	// Display menu once scripts have loaded
+	$('#main_menu').fadeIn();
+})(jQuery);
