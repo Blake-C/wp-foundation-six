be0138ab342f127d2f6b0a7652d562ff58d1e6e6
Blake Cerecero <Blake@DigitalBlake.com>
2016-12-06 14:36:33 -0600

    [lock, json, js] Added SCSS globing
    Signed-off-by: Blake Cerecero <Blake@DigitalBlake.com>

--- wp-content/themes/wp-foundation-six/gulpfile.babel.js
+++ wp-content/themes/wp-foundation-six/gulpfile.babel.js
@@ -25,6 +25,7 @@ gulp.task('styles', ['lint:sass'], () => {
 	return gulp.src(`${dir.theme_components}/sass/**/*.scss`)
 		.pipe($.plumber())
 		.pipe($.sourcemaps.init())
+		.pipe($.sassGlob())
 		.pipe($.sass.sync({
 			outputStyle: 'compact',
 			precision: 10
