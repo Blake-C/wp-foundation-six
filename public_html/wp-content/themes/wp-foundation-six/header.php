e1706b179f9557736faf80d510f801eace48ecaa
Blake Cerecero <Blake@DigitalBlake.com>
2017-03-17 01:55:22 -0500

    [php] Removed IE checks from html tag

--- public_html/wp-content/themes/wp-foundation-six/header.php
+++ public_html/wp-content/themes/wp-foundation-six/header.php
@@ -5,12 +5,7 @@
  * @package wp_foundation_six
  */
 ?><!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!-->
 <html <?php language_attributes(); ?> class="no-js">
-<!--<![endif]-->
 
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
