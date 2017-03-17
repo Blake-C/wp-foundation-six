9957e36ca8cbced1a69dd809a05da0c27c999b53
Blake Cerecero <Blake@DigitalBlake.com>
2017-03-17 01:59:50 -0500

    [php] Removed wp_foundation_six_add_async function, it was never used

--- public_html/wp-content/themes/wp-foundation-six/inc/utility-functions.php
+++ public_html/wp-content/themes/wp-foundation-six/inc/utility-functions.php
@@ -6,27 +6,6 @@
  */
 
 /**
- * Add async to js URLs
- * Only use asyn on scripts that wont break if they are loaded after
- * the page or other scripts have loaded.
- *
- * @link http://stackoverflow.com/questions/18944027/how-do-i-defer-or-async-this-wordpress-javascript-snippet-to-load-lastly-for-fas
- */
-if ( !function_exists('wp_foundation_six_add_async') ){
-	function wp_foundation_six_add_async( $url ) {
-		if ( strpos($url, '#async' ) === false ) {
-			return $url;
-		} else if ( is_admin() ) {
-			return str_replace( '#async', '', $url );
-		} else {
-			return str_replace( '#async', '', $url ) . "' async='async";
-		}
-	}
-}
-add_filter( 'clean_url', 'wp_foundation_six_add_async', 11, 1 );
-
-
-/**
  * Custom function that helps to identify the template parts of the theme.
  *
 */
