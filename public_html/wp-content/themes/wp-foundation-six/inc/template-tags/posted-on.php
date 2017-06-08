8fcc26294ab095cb2163aa219b08e2053d19e6d0
Blake Cerecero <Blake@DigitalBlake.com>
2017-06-07 23:24:52 -0500

    [php] Cleaned up posted on template tag
    Signed-off-by: Blake Cerecero <Blake@DigitalBlake.com>

--- public_html/wp-content/themes/wp-foundation-six/inc/template-tags/posted-on.php
+++ public_html/wp-content/themes/wp-foundation-six/inc/template-tags/posted-on.php
@@ -5,28 +5,38 @@ if ( ! function_exists( 'wp_foundation_six_posted_on' ) ) {
 	 * Prints HTML with meta information for the current post-date/time and author.
 	 */
 	function wp_foundation_six_posted_on() {
-		$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
-		if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-			$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
-		}
-
+		// Posted on
+		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time>';
 		$time_string = sprintf( $time_string,
 			esc_attr( get_the_date( 'c' ) ),
-			esc_html( get_the_date() ),
-			esc_attr( get_the_modified_date( 'c' ) ),
-			esc_html( get_the_modified_date() )
+			esc_html( get_the_date() )
 		);
-
 		$posted_on = sprintf(
-			esc_html_x( 'Posted on %s', 'post date', 'wp-foundation-six' ),
-			'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
+			esc_html_x( 'Posted: %s', 'post date', 'wp-foundation-six' ), $time_string
 		);
+		$posted_on_string = '<span class="posted-on post-meta">' . $posted_on . '</span>';
 
+		// Updated on
+		$updated_on_string = '';
+		if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
+			$time_string_updated = '<time class="updated" datetime="%1$s">%2$s</time>';
+			$time_string_updated = sprintf( $time_string_updated,
+				esc_attr( get_the_modified_date( 'c' ) ),
+				esc_html( get_the_modified_date() )
+			);
+			$updated_on = sprintf(
+				esc_html_x( 'Updated: %s', 'updated date', 'wp-foundation-six' ), $time_string_updated
+			);
+
+			$updated_on_string = ' <span class="updated-on post-meta">' . $updated_on . '</span>';
+		}
+
+		// Author
 		$byline = sprintf(
 			esc_html_x( 'by %s', 'post author', 'wp-foundation-six' ),
 			'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
 		);
 
-		echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
+		echo '<p>' . $posted_on_string . $updated_on_string . '<span class="byline post-meta"> ' . $byline . '</span></p>'; // WPCS: XSS OK.
 	}
 }
