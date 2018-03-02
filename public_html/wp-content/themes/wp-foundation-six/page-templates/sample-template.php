2780b1cd082e1a6e90f78eec70993ce106d9354d
Blake Cerecero <Blake@DigitalBlake.com>
2018-03-01 19:38:41 -0600

    [php] Added comments regarding theme translations
    Signed-off-by: Blake Cerecero <Blake@DigitalBlake.com>

--- public_html/wp-content/themes/wp-foundation-six/page-templates/sample-template.php
+++ public_html/wp-content/themes/wp-foundation-six/page-templates/sample-template.php
@@ -3,6 +3,13 @@
  * Template Name: Sample Page Template
  *
  * @package wp_foundation_six
+ *
+ * Be sure to properly translate any hard coded strings using
+ * a tranlation function in case the theme needs to be
+ * translated.
+ *
+ * @link https://codex.wordpress.org/I18n_for_WordPress_Developers
+ * @link https://codex.wordpress.org/Function_Reference/esc_html_x
  */
 
 get_header(); ?>
@@ -10,7 +17,7 @@ get_header(); ?>
 	<?php wp_foundation_six_dev_helper( pathinfo( __FILE__, PATHINFO_FILENAME ) ); ?>
 
 	<div class="row columns">
-		<h2>Hello World</h2>
+		<h2><?php echo esc_html_x( 'Hello World', 'Sample page template greeting', 'wp_foundation_six' ); ?></h2>
 	</div>
 
 <?php get_footer(); ?>
