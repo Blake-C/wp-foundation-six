5872aed2f2bbf42f581dea12270302947b009421
Blake Cerecero <Blake@DigitalBlake.com>
2018-08-12 16:11:31 -0500

    [php] Updated responsive embed classes to just video sites
    Signed-off-by: Blake Cerecero <Blake@DigitalBlake.com>

--- public_html/wp-content/themes/wp-foundation-six/inc/components/embed-video-container.php
+++ public_html/wp-content/themes/wp-foundation-six/inc/components/embed-video-container.php
@@ -10,11 +10,19 @@ if ( ! function_exists( 'wp_foundation_six_embed_video_html' ) ) {
 	 * Adds responsive classes to video embeds.
 	 *
 	 * @method wp_foundation_six_embed_video_html
-	 * @param  string $html - Media HTML.
-	 * @return string - New media HTML
+	 * @param  string $html Media HTML.
+	 * @param  string $url  The attempted embed URL.
+	 * @return string       New media HTML
 	 */
-	function wp_foundation_six_embed_video_html( $html ) {
-		return '<div class="responsive-embed widescreen">' . $html . '</div>';
+	function wp_foundation_six_embed_video_html( $html, $url ) {
+		$youtube = strpos( $url, 'youtube.com' );
+		$vimeo   = strpos( $url, 'vimeo.com' );
+
+		if ( false !== $youtube || false !== $vimeo ) {
+			return '<div class="responsive-embed widescreen">' . $html . '</div>';
+		} else {
+			return $html;
+		}
 	}
 }
 add_filter( 'embed_oembed_html', 'wp_foundation_six_embed_video_html', 10, 3 );
