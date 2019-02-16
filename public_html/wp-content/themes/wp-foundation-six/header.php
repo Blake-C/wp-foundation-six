17b7ed3c545b3ae9f3c910146d34f2002bc16a72
Blake Cerecero <Blake@DigitalBlake.com>
2019-02-16 02:55:09 -0600

    [php] Removed the site description and title

--- public_html/wp-content/themes/wp-foundation-six/header.php
+++ public_html/wp-content/themes/wp-foundation-six/header.php
@@ -29,27 +29,15 @@
 	<!-- https://developer.mozilla.org/en-US/docs/Mozilla/Mobile/Viewport_meta_tag -->
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<link rel="apple-touch-icon" sizes="57x57" href="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/icons/apple-touch-icon-57x57.png">
-	<link rel="apple-touch-icon" sizes="60x60" href="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/icons/apple-touch-icon-60x60.png">
-	<link rel="apple-touch-icon" sizes="72x72" href="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/icons/apple-touch-icon-72x72.png">
-	<link rel="apple-touch-icon" sizes="76x76" href="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/icons/apple-touch-icon-76x76.png">
-	<link rel="apple-touch-icon" sizes="114x114" href="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/icons/apple-touch-icon-114x114.png">
-	<link rel="apple-touch-icon" sizes="120x120" href="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/icons/apple-touch-icon-120x120.png">
-	<link rel="apple-touch-icon" sizes="144x144" href="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/icons/apple-touch-icon-144x144.png">
-	<link rel="apple-touch-icon" sizes="152x152" href="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/icons/apple-touch-icon-152x152.png">
-	<link rel="apple-touch-icon" sizes="180x180" href="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/icons/apple-touch-icon-180x180.png">
-	<link rel="icon" type="image/png" href="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/icons/favicon-32x32.png" sizes="32x32">
-	<link rel="icon" type="image/png" href="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/icons/favicon-194x194.png" sizes="194x194">
-	<link rel="icon" type="image/png" href="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/icons/favicon-96x96.png" sizes="96x96">
-	<link rel="icon" type="image/png" href="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/icons/android-chrome-192x192.png" sizes="192x192">
-	<link rel="icon" type="image/png" href="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/icons/favicon-16x16.png" sizes="16x16">
-	<link rel="manifest" href="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/icons/manifest.json">
+	<link rel="apple-touch-icon" sizes="180x180" href="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/icons/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/icons/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/icons/favicon-16x16.png">
+	<link rel="manifest" href="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/icons/site.webmanifest">
 	<link rel="mask-icon" href="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/icons/safari-pinned-tab.svg" color="#5bbad5">
 	<link rel="shortcut icon" href="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/icons/favicon.ico">
-	<meta name="msapplication-TileColor" content="#da532c">
-	<meta name="msapplication-TileImage" content="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/icons/mstile-144x144.png">
+	<meta name="msapplication-TileColor" content="#232c97">
 	<meta name="msapplication-config" content="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/icons/browserconfig.xml">
-	<meta name="theme-color" content="#1c2185">
+	<meta name="theme-color" content="#ffffff">
 
 	<!-- App Title -->
 	<meta name="apple-mobile-web-app-title" content="<?php bloginfo( 'name' ); ?>">
@@ -60,16 +48,6 @@
 
 <body <?php body_class(); ?>>
 	<a class="skip-link show-for-sr" href="#content"><?php echo esc_html_x( 'Skip to content', 'Link to jump to div with main content', 'wp_foundation_six' ); ?></a>
-
-	<?php if ( get_bloginfo( 'name' ) ) : ?>
-		<h1 class="show-for-sr"><?php bloginfo( 'name' ); ?></h1>
-	<?php endif; ?>
-
-	<?php if ( is_front_page() ) : ?>
-		<?php if ( get_bloginfo( 'description' ) ) : ?>
-			<p class="show-for-sr"><?php bloginfo( 'description' ); ?></p>
-		<?php endif; ?>
-	<?php endif; ?>
 
 	<!--[if lt IE 9]><p class=chromeframe>Your browser is <em>not</em> supported. <a href="http://browsehappy.com/">Upgrade to a different browser</a> to experience this site.</p><![endif]-->
 
