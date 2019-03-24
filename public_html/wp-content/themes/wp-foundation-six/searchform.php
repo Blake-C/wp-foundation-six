4d88a876ed3150d26441fcb6709a96277ec9d4c9
Blake Cerecero <Blake@DigitalBlake.com>
2019-03-24 00:32:18 -0500

    [php] Adjusted search form for accessibility purposes

--- public_html/wp-content/themes/wp-foundation-six/searchform.php
+++ public_html/wp-content/themes/wp-foundation-six/searchform.php
@@ -11,16 +11,66 @@
 
 <?php wp_foundation_six_dev_helper( pathinfo( __FILE__, PATHINFO_FILENAME ) ); ?>
 
-<h2 class="show-for-sr"><?php echo esc_html_x( 'Search Bar', 'Heading for search area, for screen readers', 'wp_foundation_six' ); ?></h2>
+<h2 class="show-for-sr">
+	<?php
+	echo esc_html_x(
+		'Search Bar',
+		'Heading for search area, for screen readers',
+		'wp_foundation_six'
+	);
+	?>
+</h2>
 
-<form role="search" method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+<form
+	role="search"
+	method="get"
+	id="searchform"
+	class="searchform"
+	action="<?php echo esc_url( home_url( '/' ) ); ?>"
+>
 	<div class="row collapse">
 		<div class="small-8 columns">
-			<label class="show-for-sr" for="s"><?php echo esc_html_x( 'Search', 'Search field label text', 'wp_foundation_six' ); ?></label>
-			<input type="text" placeholder="<?php echo esc_attr_x( 'Search...', 'Search field placeholder attribute', 'wp_foundation_six' ); ?>" value="<?php echo get_search_query(); ?>" name="s" id="s" />
+			<?php
+			$wpfs_search_label = _x(
+				'Search',
+				'Search field label text',
+				'wp_foundation_six'
+			);
+
+			$wpfs_search_placeholder = esc_attr_x(
+				'Search...',
+				'Search field placeholder attribute',
+				'wp_foundation_six'
+			);
+			?>
+
+			<label>
+				<span class="show-for-sr">
+					<?php echo esc_html( $wpfs_search_label ); ?>
+				</span>
+
+				<input
+					type="text"
+					placeholder="<?php echo esc_attr( $wpfs_search_placeholder ); ?>"
+					value="<?php echo get_search_query(); ?>"
+					name="s"
+				/>
+			</label>
+
 		</div>
+
 		<div class="small-4 columns">
-			<input type="submit" class="button expanded" id="searchsubmit" value="<?php echo esc_attr_x( 'Search', 'Search button value attribute', 'wp_foundation_six' ); ?>" />
+			<?php
+			$wpfs_search_value = esc_attr_x(
+				'Search',
+				'Search button value attribute',
+				'wp_foundation_six'
+			);
+			?>
+
+			<button type="submit" class="button expanded" id="search_submit">
+				<?php echo esc_html( $wpfs_search_value ); ?>
+			</button>
 		</div>
 	</div>
 </form>
